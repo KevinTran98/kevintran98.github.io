33b2f4a15f9936969c08a3ef02fd7e5208ffa661
Kevin Tran <Kevin.Tran-2@ou.edu>
2020-11-30 04:55:29 -0600

    Update

---
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Some information about me.";
+
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            ViewBag.Message = "Some projects I have worked on.";
+
+            return View();
+        }
+
+        public ActionResult SkillSet()
+        {
+            ViewBag.Message = "Skills I have gathered over the years."
+            
+            return View();
+        }
+
+        public ActionResult Employment()
+        {
+            ViewBag.Message = "Employment History"
+            
+            return View();
+        }
+    }
+}
